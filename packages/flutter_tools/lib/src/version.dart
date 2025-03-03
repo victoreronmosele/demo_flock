e2e03958c64a5e2540498cd2b4e7416faf0c1398
Victor Eronmosele <victoreronmosele@gmail.com>
2025-03-03 12:21:17 +0100

    Update version.dart with fork

--- packages/flutter_tools/lib/src/version.dart
+++ packages/flutter_tools/lib/src/version.dart
@@ -733,6 +733,9 @@ class VersionUpstreamValidator {
     'https://github.com/join-the-flock/flock.git',
     'git@github.com:join-the-flock/flock.git',
     'ssh://git@github.com/join-the-flock/flock.git',
+    'https://github.com/victoreronmosele/demo_flock.git',
+    'git@github.com:victoreronmosele/demo_flock.git',
+    'ssh://git@github.com/victoreronmosele/demo_flock.git',
   ];
 
   // Strips ".git" suffix from a given string, preferably an url.
@@ -989,7 +992,7 @@ class GitTagVersion {
         globals.printTrace('Skipping request to fetchTags - on well known channel $channel.');
       } else {
         final String flutterGit =
-            platform.environment['FLUTTER_GIT_URL'] ?? 'https://github.com/join-the-flock/flock.git';
+            platform.environment['FLUTTER_GIT_URL'] ?? 'https://github.com/victoreronmosele/demo_flock.git';
         _runGit('git fetch $flutterGit --tags -f', processUtils, workingDirectory);
       }
     }
